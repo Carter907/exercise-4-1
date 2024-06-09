9c983b1f0de9bbed57d28f75ad8327074559fdf0
Carter <102479896+Carter907@users.noreply.github.com>
2024-06-09 23:42:41 +0000

    Attempted Solution Using Enumerable
    Used Range Method and Where Method in order to get a List of factors.

--- main.cs
+++ main.cs
@@ -1,7 +1,14 @@
 using System;
+using System.Linq;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine("please enter a number");
+    var num = Convert.ToInt32(Console.ReadLine());
+    var factors = Enumerable.Range(1, num).Where((n) => num % n == 0).ToList();
+
+    foreach (var n in factors) {
+      Console.WriteLine(n);
+    }
   }
 }
